9181b72d4dba38985750be2faf229edf7481ac8b
ThuDung213 <tranthudung21032003@gmail.com>
2025-10-30 22:07:53 +0700

    feat: add cors

--- api/main.py
+++ api/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from core.rag_agent import get_agent_response
 
 app = FastAPI(title="Danang History Agent API")
 
+origins = [
+    "http://localhost:5173", 
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Cho phép các nguồn gốc này
+    allow_credentials=True,
+    allow_methods=["*"],              # RẤT QUAN TRỌNG: Cho phép tất cả phương thức, bao gồm OPTIONS
+    allow_headers=["*"],              # Cho phép tất cả các Header (bao gồm Content-Type)
+)
 class Question(BaseModel):
     question: str
     
