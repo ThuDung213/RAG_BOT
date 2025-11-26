cd565f2b1d9f6bd41bbabddf1779aef79c9aba88
Nhannn277 <nhannt2.21it@vku.udn.vn>
2025-11-26 20:35:06 +0700

    add data

--- core/agent/file_reader.py
+++ core/agent/file_reader.py
@@ -2,7 +2,8 @@ from langchain_community.document_loaders import JSONLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from core.data_utils import load_and_clean_json
+from core.agent.data_utils import load_and_clean_json
+# from core.data_utils import load_and_clean_json
 
 from dotenv import load_dotenv
 import json
